7cbefb3ea096a2269da4193bb7d5a09ebe844c25
perkles <leoazevedo1450@gmail.com>
2022-02-17 23:51:11 -0300

    Exer

--- js-intermediario-DevQuest/06-Arrays/index.js
+++ js-intermediario-DevQuest/06-Arrays/index.js
@@ -1,10 +1,10 @@
-//Criando arrays de filmes, pode ter outro tipo de dados exlet avaliacoes = [1, 4.6, 5] 
+//Criando arrays de filmes, pode ter outro tipo de dados ex: let avaliacoes = [1, 4.6, 5] 
 let filmes = ['Vingadores', 'Mulher Maravilha']
 //acessando os itens dentro do arrays 
 //console.log(nome da variável e a posição, o arrays começa sempre no indice 0)
 console.log(filmes[0])//imprimindo o primeiro elemento Vingadores
- console.log(filmes[1]) //imprimindo o segundo filme 
- console.log(filmes) //imprimi todos os elementos dentro do arrays 
+console.log(filmes[1]) //imprimindo o segundo filme 
+console.log(filmes) //imprimi todos os elementos dentro do arrays 
 
 let descricaoFilme = ['vingadores', 4.5, true]
 
