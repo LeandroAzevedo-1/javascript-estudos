5b665aeb6b92163d2f945e25a9a8929d48c0ca07
perkles <leoazevedo1450@gmail.com>
2022-02-23 23:53:44 -0300

    Form

--- Exercicios-js/formularios/formulario-login/index.js
+++ Exercicios-js/formularios/formulario-login/index.js
@@ -1,5 +1,7 @@
 const email = document.getElementById('email')
 const password = document.getElementById('password')
+const nome = document.getElementById('nome')
+const submitButton = getElementById('submite-button')
 
 email.addEventListener('focus',() =>{
     email.style.borderColor="#4a5f6a";
@@ -7,3 +9,7 @@ email.addEventListener('focus',() =>{
 password.addEventListener('focus',() =>{
     password.style.borderColor="#4a5f6a";
 })
+nome.addEventListener('focus',() =>{
+    nome.style.borderColor="#4a5f6a";
+})
+
