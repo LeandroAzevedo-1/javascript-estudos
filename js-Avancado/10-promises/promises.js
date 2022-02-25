3f69921dc370bc74efaac56a9a17550e8ed75d3a
perkles <leoazevedo1450@gmail.com>
2022-02-25 14:36:37 -0300

    promise

---
+++ js-Avancado/10-promises/promises.js
@@ -0,0 +1,34 @@
+/*
+    Promises tem 4 esdado 
+
+    Pendente, A promises não foi execultada ainda 
+    Realizada,-Resolve/ muda o estado das promes - quando teve sucesso na execulsão
+    Recusada, -Reject/muda o esdado da promese- quando teve alguma falha na execulsão
+    Estabelecida, quando foi realizada ou recusada 
+
+    Promese é uma class no js  escreve com  a primeira letra maiuscula 
+    ela vai receber uma função dentro dela. 
+    -Dentro da function, (resolve, reject ) esses parâmetros são funções 
+    
+    -sintaxe de uma promese
+    new Promise((resolve, reject )=>{
+    
+    })
+*/
+let chaleiraEstaNoFogao = true
+let fogaoEstaLigado = true
+
+let ferverAgua = () => {
+    return new Promise((resolve, reject )=>{
+        if(chaleiraEstaNoFogao && fogaoEstaLigado){ //verdadeiro verdeiro entra no resovle 
+            console.log('Começando o processo de ferver água')
+            resolve()
+        }else{
+            // se let fogaoEstaLigado for false ele entra nesse fluxo
+            console.log('É necessário ligar o fogão e colocar a chaleira no fogão para ferver a água ')
+            reject()
+        }
+    })
+}
+ferverAgua(chaleiraEstaNoFogao, fogaoEstaLigado)
+console.log('Fazendo café')
