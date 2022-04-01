79fc785a41b949ed7551bc42bb4bf84a490a47a1
perkles <leoazevedo1450@gmail.com>
2022-04-01 16:21:30 -0300

    Menu

--- Exercicios-js/menuToggleAnimacaoEfe/src/js/index.js
+++ Exercicios-js/menuToggleAnimacaoEfe/src/js/index.js
@@ -1,5 +1,11 @@
 const toggle = document.getElementById('toggle') /*Pega o id criado eno html e coloca na variável*/
+const menu  = document.getElementById('menu')
 
-toggle.onclick = function(){ /* função de evento clica que embaixo vai adicionar uma classe quando clicar  */
-    toggle.classList.toggle('active'); /* Adicionando uma classe, quando clicar ela é incluida  */
+/* função de evento clica que embaixo vai adicionar uma classe quando clicar  */
+toggle.onclick = function(){ 
+
+/* Adicionando uma classe, quando clicar ela é incluida  */
+    toggle.classList.toggle('active'); 
+/** Adiciona a classe abreEfecha menu normal quando clicar no menu hamburguer  */
+    menu.classList.toggle('abreEfecha') 
 }
